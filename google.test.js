0bd9a305ce273bb0fcb3fed279bc802bc494e289
Alex <dev.alex.88@gmail.com>
2020-01-30 23:21:38 +0700

    update

--- google.test.js
+++ google.test.js
@@ -1,5 +1,5 @@
-const { Machine } = require('xstate');
-const { createModel } = require('@xstate/test');
+const { Machine } = require('xstate')
+const { createModel } = require('@xstate/test')
 const puppeteer = require('puppeteer')
 
 const googleMachine = Machine({
@@ -23,7 +23,7 @@ const googleMachine = Machine({
             },
             meta: {
                 test: async page => {
-                    expect(page.url()).toBe('https://www.google.com/');
+                    expect(page.url()).toBe('https://www.google.com/')
                 }
             }
         },
@@ -34,7 +34,7 @@ const googleMachine = Machine({
             },
             meta: {
                 test: async page => {
-                    return await page.waitFor('#resultStats');
+                    return await page.waitFor('#resultStats')
                 }
             }
         },
@@ -44,7 +44,7 @@ const googleMachine = Machine({
             },
             meta: {
                 test: async page => {
-                    expect(page.url()).toEqual(expect.not.stringContaining('https://www.google.com'));
+                    expect(page.url()).toEqual(expect.not.stringContaining('https://www.google.com'))
                 }
             }
         },
@@ -55,7 +55,7 @@ const googleMachine = Machine({
             },
             meta: {
                 test: async page => {
-                    return await page.waitFor('[data-async-context="query:calculator"]');
+                    return await page.waitFor('[data-async-context="query:calculator"]')
                 }
             }
         },
@@ -65,14 +65,14 @@ const googleMachine = Machine({
             },
             meta: {
                 test: async page => {
-                    const element = await page.$('#cwos');
-                    const text = await page.evaluate(element => element.textContent, element);
-                    expect(text).toBe('Error');
+                    const element = await page.$('#cwos')
+                    const text = await page.evaluate(element => element.textContent, element)
+                    expect(text).toBe('Error')
                 }
             }
         }
     }
-});
+})
 
 const googleModel = createModel(googleMachine).withEvents({
     VISIT_HOME: {
@@ -82,63 +82,63 @@ const googleModel = createModel(googleMachine).withEvents({
     },
     SEARCH: {
         exec: async page => {
-            await page.type('input[name=q]', 'test');
-            await page.keyboard.press('Enter');
+            await page.type('input[name=q]', 'test')
+            await page.keyboard.press('Enter')
         }
     },
     SEARCH_LUCKY: {
         exec: async page => {
-            await page.type('input[name=q]', 'vnexpress.net');
-            await page.keyboard.press('Escape');
-            await page.click('#tsf > div:nth-child(2) > div.A8SBwf > div.FPdoLc.tfB0Bf > center > input.RNmpXc');
-            await page.waitFor(4000);
+            await page.type('input[name=q]', 'vnexpress.net')
+            await page.keyboard.press('Escape')
+            await page.click('#tsf > div:nth-child(2) > div.A8SBwf > div.FPdoLc.tfB0Bf > center > input.RNmpXc')
+            await page.waitFor(4000)
         }
     },
     SEARCH_CALCULATOR: {
         exec: async page => {
-            await page.type('input[name=q]', 'calculator');
-            await page.keyboard.press('Enter');
+            await page.type('input[name=q]', 'calculator')
+            await page.keyboard.press('Enter')
         }
     },
     ZERO_ON_ZERO: {
         exec: async page => {
-            await page.click('#cwmcwd > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(5) > td:nth-child(1) > div > div');
-            await page.click('#cwmcwd > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(2) > td:nth-child(4) > div > div');
-            await page.click('#cwmcwd > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(5) > td:nth-child(1) > div > div');
-            await page.click('#cwmcwd > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(5) > td:nth-child(3) > div > div');
-            await page.waitFor(4000);
+            await page.click('#cwmcwd > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(5) > td:nth-child(1) > div > div')
+            await page.click('#cwmcwd > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(2) > td:nth-child(4) > div > div')
+            await page.click('#cwmcwd > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(5) > td:nth-child(1) > div > div')
+            await page.click('#cwmcwd > div > div > div.SKWP2e > div > table.ElumCf > tbody > tr:nth-child(5) > td:nth-child(3) > div > div')
+            await page.waitFor(4000)
         }
     }
-});
+})
 
-jest.setTimeout(100000);
+jest.setTimeout(100000)
 
 describe('google', () => {
 
-    const testPlans = googleModel.getShortestPathPlans();
+    const testPlans = googleModel.getShortestPathPlans()
 
-    let page;
-    let browser;
+    let page
+    let browser
 
     beforeAll(async () => {
-        browser = await puppeteer.launch({headless: false});
-        page = await browser.newPage();
-        await page.setViewport({ width: 1024, height: 768});
+        browser = await puppeteer.launch({headless: false})
+        page = await browser.newPage()
+        await page.setViewport({ width: 1024, height: 768})
     })
 
     afterAll(async () => {
-        browser.close();
+        browser.close()
     })
     
     testPlans.forEach(plan => {
         plan.paths.forEach(path => {
             test(path.description, async () => {
-                await path.test(page);
-            });
-        });
-    });
+                await path.test(page)
+            })
+        })
+    })
 
     test('should have full coverage', () => {
-        return googleModel.testCoverage();
-    });
-});
+        return googleModel.testCoverage()
+    })
+})
