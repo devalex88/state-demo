aac5a67367dccea1a10a19d387db46e74bbf224b
Alex <dev.alex.88@gmail.com>
2020-01-31 16:54:06 +0700

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const findAllPaths = require('./paths')
 
 const app = express()
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 
